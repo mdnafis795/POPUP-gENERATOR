3853ccd9b6bc0c933a14904fe270d3f4537717f6
Md_Nafis <mdnafis841427@gmail.com>
2024-12-20 23:57:12 +0530

    This is pop up screen when you submit than it show message you are submit successful

---
+++ script.js
@@ -0,0 +1,8 @@
+const POPUP = document.getElementById("PopUP");
+function openpopup(){
+    POPUP.classList.add("open-popup");
+}
+
+function CLOSEPOPUP(){
+    POPUP.classList.remove("open-popup");
+}
